51c50c8794d2d5f681904e037f53f9bd3ce87d65
Cohlter <92835600+Cohlter@users.noreply.github.com>
2026-04-09 10:08:42 -0400

    Add files via upload

--- tick_tock_no.java
+++ tick_tock_no.java
@@ -1,11 +1,21 @@
 public class tick_tock_no {
 
-    int[][] nine = new int[3][3];
+    
     
 }
 
 public static void main(String[] args) {
+    int rows = 3;
+    int cols = 3;
+    int[][] nine = new int[rows][cols];
+    int value = 0;
 
-    System.out.println("Kuhn Is Gay, But Lwk Fine");
+    for(int i = 0; i < rows; i++){
+        for(int j = 0; j < cols; j++){
+            value++;
+            nine[i][j] = value;
+            System.out.println(nine[i][j]);
+        }
+    }
 
 }
