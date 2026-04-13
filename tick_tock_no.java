335a15de2a3916e56c239ad7a0519ec8d80b199b
Cohlter <92835600+Cohlter@users.noreply.github.com>
2026-04-13 10:05:26 -0400

    Add files via upload
    Locked in hard today so far! Caught up to completion of player input and setting.

--- tick_tock_no.java
+++ tick_tock_no.java
@@ -1,21 +1,62 @@
+import java.util.Scanner;
+
 public class tick_tock_no {
 
-    
-    
-}
+    public static void printBoard(char[][] board) {
 
-public static void main(String[] args) {
-    int rows = 3;
-    int cols = 3;
-    int[][] nine = new int[rows][cols];
-    int value = 0;
+        int rows = board.length;
+        int cols = board[0].length;
 
-    for(int i = 0; i < rows; i++){
-        for(int j = 0; j < cols; j++){
-            value++;
-            nine[i][j] = value;
-            System.out.println(nine[i][j]);
+        for(int i = 0; i < rows; i++){
+            for(int j = 0; j < cols; j++){
+                System.out.print(" " + board[i][j]);
+            }
+            System.out.println("");
         }
+
     }
 
+    public static char game_loop() {
+
+        System.out.println("You are playing X.");
+        System.out.print("What spot would you like to place it? ");
+
+        Scanner user = new Scanner(System.in);
+
+        char no = user.next().charAt(0);
+
+        user.close();
+
+        return no;
+
+    }
+
+    public static void place(char[][] board, char place) {
+
+        int rows = board.length;
+        int cols = board[0].length;
+
+        for(int i = 0; i < rows; i++){
+            for(int j = 0; j < cols; j++){
+                if (board[i][j] == place) {
+                    board [i][j] = 'X';
+                }
+            }
+
+        }
+
+    }
+
+    public static void main(String[] args) {
+        char[][] board = {
+            {'1', '2', '3'}, {'4', '5', '6'}, {'7', '8', '9'}
+        };
+
+        printBoard(board);
+
+        place(board, game_loop());
+
+        printBoard(board);
+    }
+    
 }
