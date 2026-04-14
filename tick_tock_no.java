49f2974f491f48149576a12bd9e3d40e6a2b49ab
Cohlter <92835600+Cohlter@users.noreply.github.com>
2026-04-14 15:31:28 -0400

    Add files via upload
    some prog

--- tick_tock_no.java
+++ tick_tock_no.java
@@ -4,6 +4,10 @@ public class tick_tock_no {
 
     public static boolean win = false;
 
+    // public static char[] filed;
+
+    //print board
+
     public static void printBoard(char[][] board) {
 
         int rows = board.length;
@@ -18,7 +22,9 @@ public class tick_tock_no {
 
     }
 
-    public static char game_loop() {
+    // gets the player's input
+
+    public static char player_spot() {
 
         if (win == true) {
             return 'Z';
@@ -35,44 +41,46 @@ public class tick_tock_no {
             user.close();
         }
 
+        // filed.
+
         return no;
 
     }
 
-    public static void place(char[][] board, char place) {
+    // gets the enemy's spot
+
+    public static char enemy_spot(char[][] board) {
+
+        int value = (int) Math.round(Math.random()*10);
+        char enemy = Integer.toString(value).charAt(0);
+        return enemy;
+
+    }
+
+    // places the player's and enemy's spots in the array
+
+
+    public static void place(char[][] board, char spot, int turn) {
 
         if (win == true) {
             return;
         }
 
-        // player
+        char marker;
+        if (turn % 2 != 0) {
+            marker = 'X';
+        } else {
+            marker = 'O';
+        }
 
         int rows = board.length;
         int cols = board[0].length;
 
-        // int enemy = -1;
-
-        // while (enemy <= 0) 
-        for(int i = 0; i < rows; i++){
-            for(int j = 0; j < cols; j++){
-                if (board[i][j] == place) {
-                    board [i][j] = 'X';
-                }
-            }
-
-        }
-
-        // enemy
-
-        int value = (int) Math.round(Math.random()*10);
-        char enemy = Integer.toString(value).charAt(0);
-
-        System.out.println(enemy);
 
         for(int i = 0; i < rows; i++){
             for(int j = 0; j < cols; j++){
-                if (board[i][j] == enemy) {
-                    board [i][j] = 'O';
+                if (board[i][j] == spot) {
+                    board [i][j] = marker;
                 }
             }
 
@@ -80,9 +88,25 @@ public class tick_tock_no {
 
     }
 
+    // 
+
     public static void check_winner(char[][] board){
 
-        if (board[0][0] == board [0][1] &&  board [0][1] == board [0][2]) {
+        if (board[0][0] == board [0][1] &&  board [0][1] == board [0][2]) { // rows
+            win = true;
+        } else if (board[1][0] == board [1][1] &&  board [1][1] == board [1][2]) {
+            win = true;
+        } else if (board[2][0] == board [2][1] &&  board [2][1] == board [2][2]) {
+            win = true;
+        } else if (board[0][0] == board [1][0] &&  board [1][0] == board [2][0]) { // columns
+            win = true;
+        } else if (board[0][1] == board [1][1] &&  board [1][1] == board [2][1]) {
+            win = true;
+        } else if (board[0][2] == board [1][2] &&  board [1][2] == board [2][2]) {
+            win = true;
+        } else if (board[0][0] == board [1][1] &&  board [1][1] == board [2][2]) { // diagonals
+            win = true;
+        } else if (board[0][2] == board [1][1] &&  board [1][1] == board [2][0]) {
             win = true;
         }
 
@@ -93,14 +117,19 @@ public class tick_tock_no {
             {'1', '2', '3'}, {'4', '5', '6'}, {'7', '8', '9'}
         };
 
+        int turn = 0;
+
         while (win == false) {
 
             printBoard(board);
 
             check_winner(board);
 
-            place(board, game_loop());
+            turn++;
+            place(board, player_spot(), turn);
 
+            turn++;
+            place(board, enemy_spot(board), turn);
         }
 
         System.out.println("You win!");
